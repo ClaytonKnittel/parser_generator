aa8779f96b128125939ec19bf9a924248994b997
Clayton Knittel <cknit1999@gmail.com>
2026-03-20 18:31:29 -0700

    Test vocab

--- lr_table/src/vocabulary.rs
+++ lr_table/src/vocabulary.rs
@@ -24,7 +24,7 @@ impl Label for TokenId {
 pub type AugmentedTokenId = AugmentedVocabToken<TokenId>;
 
 pub struct VocabularyBuilder<T> {
-  ordinal_map: HashMap<T, TokenId>,
+  token_map: HashMap<T, TokenId>,
   id_map: Vec<T>,
 }
 
@@ -32,8 +32,8 @@ impl<T: Clone + Eq + Hash> VocabularyBuilder<T> {
   pub fn get_id_or_insert(&mut self, token: AugmentedVocabToken<T>) -> TokenId {
     match token {
       AugmentedVocabToken::Token(token) => {
-        let next_id = TokenId(self.ordinal_map.len());
-        match self.ordinal_map.entry(token.clone()) {
+        let next_id = TokenId(self.token_map.len());
+        match self.token_map.entry(token.clone()) {
           Entry::Occupied(entry) => *entry.get(),
           Entry::Vacant(entry) => {
             self.id_map.push(token);
@@ -55,14 +55,14 @@ impl<T: Clone + Eq + Hash> VocabularyBuilder<T> {
 impl<T> VocabularyBuilder<T> {
   pub fn new() -> Self {
     Self {
-      ordinal_map: HashMap::new(),
+      token_map: HashMap::new(),
       id_map: Vec::new(),
     }
   }
 
   pub fn build(self) -> AugmentedVocab<T> {
     AugmentedVocab {
-      ordinal_map: self.ordinal_map,
+      token_map: self.token_map,
       id_map: self.id_map,
     }
   }
@@ -75,7 +75,7 @@ impl<T> Default for VocabularyBuilder<T> {
 }
 
 pub struct AugmentedVocab<T> {
-  ordinal_map: HashMap<T, TokenId>,
+  token_map: HashMap<T, TokenId>,
   id_map: Vec<T>,
 }
 
@@ -83,26 +83,15 @@ impl<T> AugmentedVocab<T> {
   pub fn size(&self) -> usize {
     self.id_map.len() + 2
   }
-}
-
-impl<T: Clone> AugmentedVocab<T> {
-  pub fn id_to_token(&self, id: TokenId) -> T {
-    debug_assert!(id.0 >= 2);
-    self.id_map[id.0 - 2].clone()
-  }
 
   pub fn for_each_id(&self) -> impl Iterator<Item = AugmentedTokenId> {
     (0..self.size()).map(AugmentedVocabToken::from_id)
-  }
-
-  pub fn for_each(&self) -> impl Iterator<Item = T> {
-    self.id_map.iter().cloned()
   }
 }
 
 impl<T: Eq + Hash> AugmentedVocab<T> {
   pub fn token_to_id(&self, token: &T) -> TokenId {
-    *self.ordinal_map.get(token).unwrap()
+    *self.token_map.get(token).unwrap()
   }
 
   pub fn augmented_token_to_id(&self, token: &AugmentedVocabToken<T>) -> AugmentedTokenId {
@@ -172,5 +161,149 @@ impl Display for VocabSet {
 impl Debug for VocabSet {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "{}", self.set.for_each().join("/"))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use googletest::prelude::*;
+  use itertools::Itertools;
+
+  use crate::{
+    augmented_vocab_token::AugmentedVocabToken,
+    fixed_map::Label,
+    vocabulary::{AugmentedTokenId, TokenId, VocabularyBuilder},
+  };
+
+  #[gtest]
+  fn test_empty_vocab() {
+    let vocab = VocabularyBuilder::<u8>::new().build();
+    expect_eq!(
+      vocab.augmented_token_to_id(&AugmentedVocabToken::Epsilon),
+      AugmentedTokenId::Epsilon
+    );
+    expect_eq!(
+      vocab.augmented_token_to_id(&AugmentedVocabToken::EndOfStream),
+      AugmentedTokenId::EndOfStream
+    );
+
+    expect_that!(
+      vocab.for_each_id().collect_vec(),
+      unordered_elements_are![
+        &AugmentedVocabToken::Epsilon,
+        &AugmentedVocabToken::EndOfStream
+      ]
+    );
+
+    expect_that!(
+      vocab.for_each_id().map(|id| id.id()).collect_vec(),
+      unordered_elements_are![&0, &1]
+    );
+  }
+
+  #[gtest]
+  fn test_single_token_vocab() {
+    let mut builder = VocabularyBuilder::new();
+    let a_id = builder.get_id_or_insert(b'a'.into());
+    expect_eq!(a_id, TokenId(0));
+
+    let vocab = builder.build();
+    expect_eq!(
+      vocab.augmented_token_to_id(&AugmentedVocabToken::Epsilon),
+      AugmentedTokenId::Epsilon
+    );
+    expect_eq!(
+      vocab.augmented_token_to_id(&AugmentedVocabToken::EndOfStream),
+      AugmentedTokenId::EndOfStream
+    );
+    expect_eq!(vocab.augmented_token_to_id(&b'a'.into()), a_id.into());
+    expect_eq!(vocab.token_to_id(&b'a'), a_id);
+
+    expect_that!(
+      vocab.for_each_id().collect_vec(),
+      unordered_elements_are![
+        &AugmentedVocabToken::Epsilon,
+        &AugmentedVocabToken::EndOfStream,
+        &AugmentedVocabToken::Token(a_id)
+      ]
+    );
+
+    expect_that!(
+      vocab.for_each_id().map(|id| id.id()).collect_vec(),
+      unordered_elements_are![&0, &1, &2]
+    );
+  }
+
+  #[gtest]
+  fn test_three_token_vocab() {
+    let mut builder = VocabularyBuilder::new();
+    let a_id = builder.get_id_or_insert(b'a'.into());
+    let b_id = builder.get_id_or_insert(b'b'.into());
+    let c_id = builder.get_id_or_insert(b'c'.into());
+    expect_eq!(a_id, TokenId(0));
+    expect_eq!(b_id, TokenId(1));
+    expect_eq!(c_id, TokenId(2));
+
+    let vocab = builder.build();
+    expect_eq!(
+      vocab.augmented_token_to_id(&AugmentedVocabToken::Epsilon),
+      AugmentedTokenId::Epsilon
+    );
+    expect_eq!(
+      vocab.augmented_token_to_id(&AugmentedVocabToken::EndOfStream),
+      AugmentedTokenId::EndOfStream
+    );
+    expect_eq!(vocab.augmented_token_to_id(&b'a'.into()), a_id.into());
+    expect_eq!(vocab.token_to_id(&b'a'), a_id);
+    expect_eq!(vocab.augmented_token_to_id(&b'b'.into()), b_id.into());
+    expect_eq!(vocab.token_to_id(&b'b'), b_id);
+    expect_eq!(vocab.augmented_token_to_id(&b'c'.into()), c_id.into());
+    expect_eq!(vocab.token_to_id(&b'c'), c_id);
+
+    expect_that!(
+      vocab.for_each_id().collect_vec(),
+      unordered_elements_are![
+        &AugmentedVocabToken::Epsilon,
+        &AugmentedVocabToken::EndOfStream,
+        &AugmentedVocabToken::Token(a_id),
+        &AugmentedVocabToken::Token(b_id),
+        &AugmentedVocabToken::Token(c_id)
+      ]
+    );
+
+    expect_that!(
+      vocab.for_each_id().map(|id| id.id()).collect_vec(),
+      unordered_elements_are![&0, &1, &2, &3, &4]
+    );
+  }
+
+  #[gtest]
+  fn test_label_id_roundtrip() {
+    let mut builder = VocabularyBuilder::new();
+    builder.get_id_or_insert(b'a'.into());
+    builder.get_id_or_insert(b'b'.into());
+    builder.get_id_or_insert(b'c'.into());
+    builder.get_id_or_insert(b'd'.into());
+    builder.get_id_or_insert(b'e'.into());
+
+    let vocab = builder.build();
+    expect_that!(
+      vocab
+        .for_each_id()
+        .map(|id| id.id())
+        .map(AugmentedVocabToken::from_id)
+        .collect_vec(),
+      container_eq(vocab.for_each_id().collect_vec())
+    );
+  }
+
+  #[gtest]
+  fn test_insert_id_twice() {
+    let mut builder = VocabularyBuilder::new();
+    let a_id = builder.get_id_or_insert(b'a'.into());
+    let b_id = builder.get_id_or_insert(b'b'.into());
+
+    expect_eq!(builder.get_id_or_insert(b'b'.into()), b_id);
+    expect_eq!(builder.get_id_or_insert(b'a'.into()), a_id);
   }
 }
