d765e3345bfb5e47cb0382eb80f0e76d43f50fdb
Clayton Knittel <cknit1999@gmail.com>
2026-03-22 23:42:29 -0700

    Prog

--- parser_generator_impl/src/code_gen/code_gen.rs
+++ parser_generator_impl/src/code_gen/code_gen.rs
@@ -1,11 +1,14 @@
 use lr_table::{
   indexed_grammar::IndexedGrammar,
+  lr_state_map::{LRStateMap, LRStateType},
   lr_table::{LRTable, StateId},
 };
+use proc_macro::Span;
 use quote::quote;
 
 use crate::{
-  annotated_grammar::parse_grammar::GrammarInfo, type_symbol::Type, ParserGeneratorResult,
+  annotated_grammar::parse_grammar::GrammarInfo, type_symbol::Type, ParserGeneratorError,
+  ParserGeneratorResult,
 };
 
 type TokenStreamResult = ParserGeneratorResult<proc_macro2::TokenStream>;
@@ -14,7 +17,7 @@ fn generate_enum_variant(state: StateId, return_type: Option<Type>) -> proc_macr
   todo!();
 }
 
-fn generate_dfa_states<T>(
+fn generate_dfa_states<T: Clone>(
   grammar: &IndexedGrammar<T>,
   lr_table: &LRTable<T>,
   grammar_info: &GrammarInfo,
@@ -25,7 +28,24 @@ fn generate_dfa_states<T>(
     grammar_name.meta().span2(),
   );
 
-  lr_table.states();
+  let state_map = LRStateMap::build_from_lr_table(grammar, lr_table)
+    .map_err(|err| ParserGeneratorError::from_foreign_error(err, Span::call_site()))?;
+  let enums = lr_table
+    .states()
+    .map(|state| {
+      (
+        state,
+        match state_map.state_type(state) {
+          LRStateType::Reduce(production) => {
+            grammar_info.lr_table_grammar();
+            None
+          }
+          LRStateType::Terminal => Some(grammar_info.terminal_type().clone()),
+          LRStateType::Root => None,
+        },
+      )
+    })
+    .map(|(state, maybe_type)| generate_enum_variant(state, maybe_type));
 
   Ok(quote! {
     struct #dfa_enum_name {
--- parser_generator_impl/src/code_gen/mod.rs
+++ parser_generator_impl/src/code_gen/mod.rs
@@ -1,2 +1,1 @@
 pub mod code_gen;
-mod lr_state_types;
