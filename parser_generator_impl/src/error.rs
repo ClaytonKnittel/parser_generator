cb12a031c599baf8c03457af3adc9a676ad3f494
Clayton Knittel <cknit1999@gmail.com>
2026-03-29 12:11:51 -0700

    Update cknittel-util dependency

--- parser_generator_impl/src/error.rs
+++ parser_generator_impl/src/error.rs
@@ -45,31 +45,6 @@ impl Debug for ParserGeneratorError {
 
 pub type ParserGeneratorResult<T = ()> = Result<T, ParserGeneratorError>;
 
-pub trait EraseOk<E> {
-  /// Erases the value of a result, replacing it with `()`. Equivalent to
-  /// calling `.map(|_| ())` on the result.
-  fn erase_ok(self) -> Result<(), E>;
-}
-
-impl<T, E> EraseOk<E> for Result<T, E> {
-  fn erase_ok(self) -> Result<(), E> {
-    self.map(|_| ())
-  }
-}
-
-pub trait CloneErr<T, E> {
-  fn clone_err(&self) -> Result<&T, E>;
-}
-
-impl<T, E: Clone> CloneErr<T, E> for Result<T, E> {
-  fn clone_err(&self) -> Result<&T, E> {
-    match self {
-      Ok(value) => Ok(value),
-      Err(err) => Err(err.clone()),
-    }
-  }
-}
-
 pub trait InterceptResult {
   /// Appents the given string to the error in this result. This is a no-op if
   /// the result is `Ok`.
