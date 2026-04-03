9f8d595943e514aa5c8e5c7a1e3a7959bd72d96f
Clayton Knittel <cknit1999@gmail.com>
2026-04-02 23:07:24 -0700

    Comment

--- parser_generator_impl/src/code_gen/util.rs
+++ parser_generator_impl/src/code_gen/util.rs
@@ -4,16 +4,8 @@ use crate::ParserGeneratorResult;
 
 pub type TokenStreamResult = ParserGeneratorResult<proc_macro2::TokenStream>;
 
-/// A prefix to prepend to symbols that will be visible from user code (e.g.
+/// Adds a prefix to symbols that will be visible from user code (e.g.
 /// production constructors) to prevent unexpected/confusing naming conflicts.
-pub fn unique_prefix() -> &'static str {
-  "__parser_generator_"
-}
-
-pub fn add_unique_prefix(name: &str) -> String {
-  format!("{}{name}", unique_prefix())
-}
-
 pub fn unique_prefixed_ident(name: &str) -> proc_macro2::Ident {
-  proc_macro2::Ident::new(&add_unique_prefix(name), Span::call_site())
+  proc_macro2::Ident::new(&format!("__parser_generator_{name}"), Span::call_site())
 }
