ee97b0e25541c8adfe7955205de7bab143049fa0
ClaytonKnittel <35512940+ClaytonKnittel@users.noreply.github.com>
2026-04-05 02:28:13 -0700

    Update cknittel-util (#29)
    * Update cknittel-util
    
    * default try_from ctor
    
    * test try_from default ctor

---
+++ tests/try_from_default_ctor.rs
@@ -0,0 +1,68 @@
+use std::{error::Error, fmt::Display};
+
+use googletest::prelude::*;
+use parser_generator::{error::ParserError, grammar, parser::Parser};
+
+#[derive(Debug)]
+struct MyError {
+  message: String,
+}
+
+impl Error for MyError {}
+
+impl Display for MyError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", self.message)
+  }
+}
+
+#[derive(Clone, Debug)]
+struct A(u32);
+
+#[derive(Clone, Debug)]
+enum Container {
+  A(A),
+}
+
+impl TryFrom<A> for Container {
+  type Error = MyError;
+
+  fn try_from(value: A) -> std::result::Result<Self, Self::Error> {
+    if value.0 < 10 {
+      Ok(Container::A(value))
+    } else {
+      Err(MyError {
+        message: format!("Value \"{}\" cannot be greater than 9", value.0),
+      })
+    }
+  }
+}
+
+grammar!(
+  name: AutoInto;
+  terminal: char;
+
+  <root>: Container => <a>;
+
+  <a>: A => 'a' { A(1) };
+  <a>: A => 'z' { A(100) };
+);
+
+#[gtest]
+fn parse_a() {
+  let result = AutoInto::parse("a".chars());
+
+  expect_that!(result, ok(pat!(Container::A(pat!(A(eq(&1)))))));
+}
+
+#[gtest]
+fn parse_b() {
+  let result = AutoInto::parse("z".chars());
+
+  expect_that!(
+    result,
+    err(pat!(ParserError::ForeignError {
+      message: contains_substring("Value \"100\" cannot be greater than 9")
+    }))
+  );
+}
