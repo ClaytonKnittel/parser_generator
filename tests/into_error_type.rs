260c45648771ebb0fe8f14359705950ff4ad7d6b
ClaytonKnittel <35512940+ClaytonKnittel@users.noreply.github.com>
2026-04-07 22:12:21 -0700

    Allow ? to yield error types which are Into<> parser error type (#33)

--- tests/into_error_type.rs
+++ tests/into_error_type.rs
@@ -44,10 +44,8 @@ enum Container {
   A(A),
 }
 
-impl TryFrom<A> for Container {
-  type Error = MyOtherError;
-
-  fn try_from(value: A) -> std::result::Result<Self, Self::Error> {
+impl Container {
+  fn try_from_a(value: A) -> std::result::Result<Container, MyOtherError> {
     if value.0 < 10 {
       Ok(Container::A(value))
     } else {
@@ -58,35 +56,49 @@ impl TryFrom<A> for Container {
   }
 }
 
+impl TryFrom<A> for Container {
+  type Error = MyOtherError;
+
+  fn try_from(value: A) -> std::result::Result<Container, Self::Error> {
+    Container::try_from_a(value)
+  }
+}
+
 grammar!(
   name: AutoInto;
   terminal: char;
   error_type: MyError;
 
-  <root>: Container => <a>;
+  <root>: Container => <a_or_b>;
+
+  <a_or_b>: Container => <a>;
+  <a_or_b>: Container => <b>;
 
   <a>: A => 'a' { A(1) };
   <a>: A => 'z' { A(100) };
 
   <a>: A => 'b' { A(1).try_into()? };
   <a>: A => 'y' { A(100).try_into()? };
+
+  <b>: Container => 'c' { Container::try_from_a(A(1))? };
+  <b>: Container => 'x' { Container::try_from_a(A(100))? };
 );
 
 #[gtest]
 fn parse_a() {
   let result = AutoInto::parse("a".chars());
-
   expect_that!(result, ok(pat!(Container::A(pat!(A(eq(&1)))))));
 
   let result = AutoInto::parse("b".chars());
+  expect_that!(result, ok(pat!(Container::A(pat!(A(eq(&1)))))));
 
+  let result = AutoInto::parse("c".chars());
   expect_that!(result, ok(pat!(Container::A(pat!(A(eq(&1)))))));
 }
 
 #[gtest]
 fn parse_b() {
   let result = AutoInto::parse("z".chars());
-
   expect_that!(
     result,
     err(pat!(ParserError::UserError(displays_as(
@@ -95,7 +107,14 @@ fn parse_b() {
   );
 
   let result = AutoInto::parse("y".chars());
+  expect_that!(
+    result,
+    err(pat!(ParserError::UserError(displays_as(
+      contains_substring("Value \"100\" cannot be greater than 9")
+    ))))
+  );
 
+  let result = AutoInto::parse("x".chars());
   expect_that!(
     result,
     err(pat!(ParserError::UserError(displays_as(
